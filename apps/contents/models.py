5284018832ac965a45b589020f522ced5aeeff04
smk950226 <smk950226@naver.com>
2018-07-15 18:05:19 +0900

    like 모델 분리

--- apps/contents/models.py
+++ apps/contents/models.py
@@ -23,7 +23,7 @@ class Contents(models.Model):
     tags = models.CharField('태그', max_length = 200, blank = True, null = True)
     description = models.TextField('설명', blank = True, null = True)
     regist_dt = models.DateTimeField('작성 시각', auto_now_add = True)
-    like = models.PositiveIntegerField('좋아요', default = 0)
+    update_dt = models.DateTimeField('수정 시각', auto_now = True)
     views = models.PositiveIntegerField('조회수', default = 0)
 
     class Meta:
@@ -36,5 +36,12 @@ class Contents(models.Model):
 
 
 class ContentsImages(models.Model):
+    user = models.ForeignKey(User, verbose_name = '회원', on_delete = models.CASCADE)
     contents = models.ForeignKey(Contents, verbose_name = '콘텐츠', default=None, on_delete = models.CASCADE)
     image = models.ImageField(upload_to=get_image_filename, verbose_name='이미지')
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, verbose_name = '회원', on_delete = models.CASCADE)
+    contents = models.ForeignKey(Contents, verbose_name = '콘텐츠', default=None, on_delete = models.CASCADE)
+    regist_dt = models.DateTimeField('작성 시각', auto_now_add = True)
