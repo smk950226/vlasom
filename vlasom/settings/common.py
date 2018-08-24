4421a6ab06a63496d81e3839894184f87a034b24
SEUNGMIN KIM <smk950226@naver.com>
2018-08-25 00:47:36 +0900

    settings 분기

--- vlasom/settings/common.py
+++ vlasom/settings/common.py
@@ -14,7 +14,7 @@ import os, json
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 SITE_ID = 1
 
--- vlasom/wsgi.py
+++ vlasom/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vlasom.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vlasom.settings.deploy")
 
 application = get_wsgi_application()
